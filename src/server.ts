36e41335e37939d8697850664b3b0910e64dfb90
henrikesilva <henrikelima.0502@gmail.com>
2024-01-03 22:31:43 -0300

    Conexão com banco de dados; Criação das primeiras migrations; Criação e Tipagem das variaveis de ambiente;

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
 
-  return tables
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
